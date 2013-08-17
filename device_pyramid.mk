71cd42af516148f5835745653cceb2375496005b
Matt Filetto <matt.filetto@gmail.com>
2013-08-17 15:57:32 -0700

    ramdisk: sync fstab with evervolv's
    Trying to reslove booting issue with some devices
    
    Reverts to previous mount options for system and data partition and removed
    remount.pyramid
    
    Change-Id: Id94a325f9bb99d3365986555ff72beb70056bb84

--- device_pyramid.mk
+++ device_pyramid.mk
@@ -40,8 +40,7 @@ PRODUCT_COPY_FILES += \
     device/htc/pyramid/ramdisk/fstab.pyramid:root/fstab.pyramid \
     device/htc/pyramid/ramdisk/init.pyramid.rc:root/init.pyramid.rc \
     device/htc/pyramid/ramdisk/init.pyramid.usb.rc:root/init.pyramid.usb.rc \
-    device/htc/pyramid/ramdisk/ueventd.pyramid.rc:root/ueventd.pyramid.rc \
-    device/htc/pyramid/ramdisk/remount.pyramid:root/remount.pyramid 
+    device/htc/pyramid/ramdisk/ueventd.pyramid.rc:root/ueventd.pyramid.rc
 
 ## recovery and custom charging
 PRODUCT_COPY_FILES += \
