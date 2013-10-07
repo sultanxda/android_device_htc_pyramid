81db49e73b66e74b581843a3d7974a7d5b1a97fd
Sultanxda <sultanxda@gmail.com>
2013-10-06 21:53:18 -0700

    repo-patcher: add patch for HTCZ715E's WiFi region code fix

--- repo-patcher.sh
+++ repo-patcher.sh
@@ -7,4 +7,7 @@ patch -p1 < $HOME/cm10.2/device/htc/pyramid/patches/build-kernel.patch
 cd $HOME/cm10.2/packages/apps/Phone
 git reset --hard && git clean -f -d
 patch -p1 < $HOME/cm10.2/device/htc/pyramid/patches/phone-nlj-s2w-patch.patch
+cd $HOME/cm10.2/frameworks/opt/telephony
+git reset --hard && git clean -f -d
+patch -p1 < frameworks-opt-telephony.patch
 cd $HOME/cm10.2
