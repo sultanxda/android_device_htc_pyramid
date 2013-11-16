20b52d7e103b1b1cc257a2712a583bde7b501233
Sultanxda <sultanxda@gmail.com>
2013-11-16 12:50:12 -0800

    patches: Add USE_SET_METADATA patch
    Credit is located inside the patch. (credit: flintman)

--- repo-patcher.sh
+++ repo-patcher.sh
@@ -4,6 +4,7 @@ echo Patching each individual repo for regular building...
 cd $HOME/cm11.0/build
 git reset --hard && git clean -f -d
 patch -p1 < $HOME/cm11.0/device/htc/pyramid/patches/build-kernel.patch
+patch -p1 < $HOME/cm11.0/device/htc/pyramid/patches/build-use-set-metadata.patch
 cd $HOME/cm11.0/frameworks/opt/telephony
 git reset --hard && git clean -f -d
 patch -p1 < $HOME/cm11.0/device/htc/pyramid/patches/frameworks-opt-telephony.patch
