1c85b273d96981f1da2bff74b47c77563e9954ec
Sultanxda <sultanxda@gmail.com>
2013-12-03 16:20:57 -0800

    repo-patcher: Universal compatibility and add media provider fix

--- repo-patcher.sh
+++ repo-patcher.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 echo Patching each individual repo for regular building...
-cd $HOME/cm11.0/build
+cd build
 git reset --hard && git clean -f -d
 patch -p1 < $HOME/cm11.0/device/htc/pyramid/patches/build-kernel.patch
 patch -p1 < $HOME/cm11.0/device/htc/pyramid/patches/build-use-set-metadata.patch
-cd $HOME/cm11.0/frameworks/opt/telephony
+cd ../frameworks/opt/telephony
 git reset --hard && git clean -f -d
-patch -p1 < $HOME/cm11.0/device/htc/pyramid/patches/frameworks-opt-telephony.patch
-cd $HOME/cm11.0/packages/apps/Torch
+patch -p1 < ../../../device/htc/pyramid/patches/frameworks-opt-telephony.patch
+cd ../../../packages/apps/Torch
 git reset --hard && git clean -f -d
-patch -p1 < $HOME/cm11.0/device/htc/pyramid/patches/packages-apps-torch.patch
-cd $HOME/cm11.0
+patch -p1 < ../../../device/htc/pyramid/patches/packages-apps-torch.patch
+cd ../../../packages/providers/MediaProvider
+git reset --hard && git clean -f -d
+patch -p1 < ../../../device/htc/pyramid/patches/mediaproviderfix.patch
+cd ../../../
