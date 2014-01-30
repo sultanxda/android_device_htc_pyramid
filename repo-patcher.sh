0771664f3cc11e45c04be3f3dc94f088e07e72b2
Sultanxda <sultanxda@gmail.com>
2014-01-29 17:29:56 -0800

    repo-patcher: Support universal building

--- repo-patcher.sh
+++ repo-patcher.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 echo Patching each individual repo for regular building...
-cd $HOME/cm10.2/build
+
+cd build
 git reset --hard && git clean -f -d
-patch -p1 < $HOME/cm10.2/device/htc/pyramid/patches/build-kernel.patch
-cd $HOME/cm10.2/packages/apps/Phone
+patch -p1 < ../device/htc/pyramid/patches/build-kernel.patch
+cd ../packages/apps/Phone
 git reset --hard && git clean -f -d
-patch -p1 < $HOME/cm10.2/device/htc/pyramid/patches/phone-nlj-s2w-patch.patch
-cd $HOME/cm10.2/frameworks/opt/telephony
+patch -p1 < ../../../device/htc/pyramid/patches/phone-nlj-s2w-patch.patch
+cd ../../../frameworks/opt/telephony
 git reset --hard && git clean -f -d
-patch -p1 < $HOME/cm10.2/device/htc/pyramid/patches/frameworks-opt-telephony.patch
-cd $HOME/cm10.2/packages/apps/Torch
+patch -p1 < ../../../device/htc/pyramid/patches/frameworks-opt-telephony.patch
+cd ../../../packages/apps/Torch
 git reset --hard && git clean -f -d
-patch -p1 < $HOME/cm10.2/device/htc/pyramid/patches/packages-apps-torch.patch
-cd $HOME/cm10.2
+patch -p1 < ../../../device/htc/pyramid/patches/packages-apps-torch.patch
+cd ../../..
