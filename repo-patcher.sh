989264f27a2ba9bed0df56ecab668ed2b2cb7cdf
Sultanxda <sultanxda@gmail.com>
2014-01-29 17:51:43 -0800

    repo-patcher: Support universal building

--- repo-patcher.sh
+++ repo-patcher.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 echo Patching each individual repo for regular building...
+
 cd build
 git reset --hard && git clean -f -d
-patch -p1 < $HOME/cm11.0/device/htc/pyramid/patches/build-patch.patch
+patch -p1 < $HOME/cm11.0/device/htc/pyramid/patches/build.patch
 cd ../frameworks/opt/telephony
 git reset --hard && git clean -f -d
 patch -p1 < ../../../device/htc/pyramid/patches/frameworks-opt-telephony.patch
 cd ../../../packages/apps/Torch
 git reset --hard && git clean -f -d
 patch -p1 < ../../../device/htc/pyramid/patches/packages-apps-torch.patch
-cd ../../../
+cd ../../..
