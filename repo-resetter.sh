088efc326ba969f1de006ac65d817b57659ae8b1
Sultanxda <sultanxda@gmail.com>
2014-02-08 14:36:41 -0800

    repo-resetter: Support universal commands

--- repo-resetter.sh
+++ repo-resetter.sh
@@ -2,12 +2,10 @@
 
 # Reset each repo in order to successfully repo sync
 echo Resetting each repo...
-cd $HOME/cm11.0/build
+cd build
 git reset --hard && git clean -f -d
-cd $HOME/cm11.0/packages/apps/Phone
+cd ../frameworks/opt/telephony
 git reset --hard && git clean -f -d
-cd $HOME/cm11.0/frameworks/opt/telephony
+cd ../../../packages/apps/Torch
 git reset --hard && git clean -f -d
-cd $HOME/cm11.0/packages/apps/Torch
-git reset --hard && git clean -f -d
-cd $HOME/cm11.0
+cd ../../../
