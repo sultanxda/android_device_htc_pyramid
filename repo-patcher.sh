bc88ad7abeb94e609f57dd6be7dad2d590a2103c
Sultanxda <sultanxda@gmail.com>
2014-02-14 21:13:04 -0800

    repo-patcher: Fix path derp

--- repo-patcher.sh
+++ repo-patcher.sh
@@ -4,7 +4,7 @@ echo Patching each individual repo for regular building...
 
 cd build
 git reset --hard && git clean -f -d
-patch -p1 < $HOME/cm11.0/device/htc/pyramid/patches/build.patch
+patch -p1 < ../device/htc/pyramid/patches/build.patch
 cd ../frameworks/opt/telephony
 git reset --hard && git clean -f -d
 patch -p1 < ../../../device/htc/pyramid/patches/frameworks-opt-telephony.patch
