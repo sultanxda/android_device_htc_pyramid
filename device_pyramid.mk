c4a25615f5f12334f3660fa4b86c8c724bdc0b54
Sultanxda <sultanxda@gmail.com>
2014-03-24 00:31:19 -0700

    Add Netflix fix by liquid64

--- device_pyramid.mk
+++ device_pyramid.mk
@@ -47,7 +47,8 @@ PRODUCT_COPY_FILES += \
 
 # Misc configuration files
 PRODUCT_COPY_FILES += \
-    device/htc/pyramid/configs/89z_kernel:system/etc/init.d/89z_kernel
+    device/htc/pyramid/configs/89z_kernel:system/etc/init.d/89z_kernel \
+    device/htc/pyramid/configs/98netflix:system/etc/init.d/98netflix
 
 # Keylayouts and Keychars
 PRODUCT_COPY_FILES += \
