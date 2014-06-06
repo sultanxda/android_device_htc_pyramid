7895edf59a42dac86d020dc0b9efe394b83224dc
Sultanxda <sultanxda@gmail.com>
2014-06-05 21:05:27 -0700

    Remove patcher
    No longer needed :)

--- repo-patcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo Patching each individual repo for regular building...
-
-cd frameworks/opt/telephony
-git reset --hard && git clean -f -d
-patch -p1 < ../../../device/htc/pyramid/patches/frameworks-opt-telephony.patch
-cd ../../..
