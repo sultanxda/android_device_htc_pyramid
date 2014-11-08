6324fa4074645eb81d8f141de944ced1dd73d905
Ivan Grinko <iivanich@gmail.com>
2014-11-08 13:22:42 +0200

    pyramid: Add custom audio policy configuration
    Removed AUDIO_OUTPUT_FLAG_VOIP_RX

--- device_pyramid.mk
+++ device_pyramid.mk
@@ -143,7 +143,6 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio_policy.msm8660 \
     audio.primary.msm8660 \
-    audio_policy.conf \
     libaudioutils \
     libaudio-resampler \
     audio.usb.default
@@ -205,6 +204,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     make_ext4fs \
     setup_fs
+
+# Audio policy configuration
+PRODUCT_COPY_FILES += \
+    device/htc/pyramid/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # Media configuration
 PRODUCT_COPY_FILES += \
